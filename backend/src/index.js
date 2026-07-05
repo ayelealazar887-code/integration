791ed716707d52937997bfd2756b789fad603aeb
ayelealazar887-code <ayelealazar887@gmail.com>
2026-07-05 22:18:52 +0300

    postgres db connetion established

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv from "dotenv"
+import pool from "./config/data.js"
 
 dotenv.config()
 
@@ -9,6 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT current_database();");
+
+    res.send(`Connected to database: ${result.rows[0].current_database}`);
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    res.status(500).send("Error connecting to the database");
+  }
+});
 
 app.listen(port, () => { 
     console.log(`server running at ${port}`);
