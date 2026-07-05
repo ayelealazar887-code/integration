d91a47d5ef920d014ddb3b6da536efaa2b9a627a
ayelealazar887-code <ayelealazar887@gmail.com>
2026-07-05 22:04:31 +0300

    initial commit

---
+++ backend/src/config/data.js
@@ -0,0 +1,21 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pkg;
+console.log(process.env.DB_USER);
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DBPORT,
+});
+
+pool.on("connect", () => {
+  console.log("Connected to the database");
+});
+
+export default pool;
